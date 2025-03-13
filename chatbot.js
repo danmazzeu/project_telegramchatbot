cb66b5743656646a72777e0a6b149faeef2c1e05
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-03-13 19:17:00 -0300

    Update

--- chatbot.js
+++ chatbot.js
@@ -2,20 +2,25 @@ const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 const menuOptions = require('./config');
 
-const token = '7750421048:AAE2LBc0gj2dLU3lejkD2LAFAG5pTEDu5RU';
+const token = 'AAE2LBc0gj2dLU3lejkD2LAFAG5pTEDu5RU';
 const bot = new TelegramBot(token, { polling: true });
 
 console.log('Bot iniciado e aguardando mensagens...');
 
 const userState = {};
 
+// Função para escapar caracteres especiais no MarkdownV2 do Telegram
+function escapeMarkdownV2(text) {
+    return text.replace(/([_*[\]()~`>#+-=|{}.!])/g, '\\$1');
+}
+
 // Exibe o menu principal
 function showMenu(chatId) {
     let menuText = '*Escolha uma opção:*\n\n';
     menuOptions.forEach(option => {
-        menuText += `[${option.option}] ${option.text}\n`;
+        menuText += `*[${option.option}]* ${escapeMarkdownV2(option.text)}\n`;
     });
-    bot.sendMessage(chatId, menuText);
+    bot.sendMessage(chatId, menuText, { parse_mode: 'MarkdownV2' });
     userState[chatId] = { menu: 'main', parent: null };
 }
 
@@ -23,13 +28,13 @@ function showMenu(chatId) {
 function showSubMenu(chatId, parentOption) {
     const selectedOption = menuOptions.find(item => item.option === parentOption);
     if (selectedOption && selectedOption.subOptions?.length > 0) {
-        let subMenuText = `*Opção selecionada:*\n[${selectedOption.option}] ${selectedOption.text}\n\nEscolha uma opção:\n`;
+        let subMenuText = `*Opção selecionada:*\n*[${selectedOption.option}]* ${escapeMarkdownV2(selectedOption.text)}\n\n*Escolha uma opção:*\n`;
         selectedOption.subOptions.forEach(sub => {
-            subMenuText += `[${sub.option}] ${sub.text}\n`;
+            subMenuText += `*[${sub.option}]* ${escapeMarkdownV2(sub.text)}\n`;
         });
-        subMenuText += `[0] Voltar ao menu principal`;
+        subMenuText += `\n*[0]* Voltar ao menu principal`;
 
-        bot.sendMessage(chatId, subMenuText);
+        bot.sendMessage(chatId, subMenuText, { parse_mode: 'MarkdownV2' });
         userState[chatId] = { menu: 'submenu', parent: parentOption };
     } else {
         bot.sendMessage(chatId, 'Opção inválida ou sem subopções.');
@@ -54,7 +59,7 @@ bot.on('message', msg => {
             if (selectedOption.subOptions?.length > 0) {
                 showSubMenu(chatId, text);
             } else {
-                bot.sendMessage(chatId, `[${selectedOption.option}] ${selectedOption.text}`);
+                bot.sendMessage(chatId, `*[${selectedOption.option}]* ${escapeMarkdownV2(selectedOption.text)}`, { parse_mode: 'MarkdownV2' });
             }
         } else {
             bot.sendMessage(chatId, 'Opção inválida. Tente novamente.');
@@ -69,7 +74,7 @@ bot.on('message', msg => {
         if (parentOption) {
             const subOption = parentOption.subOptions.find(sub => sub.option === text.toString());
             if (subOption) {
-                bot.sendMessage(chatId, `[${subOption.option}] ${subOption.text}`);
+                bot.sendMessage(chatId, `*[${subOption.option}]* ${escapeMarkdownV2(subOption.text)}`, { parse_mode: 'MarkdownV2' });
             } else {
                 bot.sendMessage(chatId, 'Opção inválida. Tente novamente.');
                 showMenu(chatId);
