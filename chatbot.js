c106fdad73573cad452216002731d9de8282e08c
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-03-13 18:48:57 -0300

    Update

--- chatbot.js
+++ chatbot.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const fs = require('fs');
 const menuOptions = require('./config');
 
-const token = 'AAE2LBc0gj2dLU3lejkD2LAFAG5pTEDu5RU';
+const token = '7750421048:AAE2LBc0gj2dLU3lejkD2LAFAG5pTEDu5RU';
 const bot = new TelegramBot(token, { polling: true });
 
 console.log('Bot iniciado e aguardando mensagens...');
