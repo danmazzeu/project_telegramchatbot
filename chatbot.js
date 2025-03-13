673cb9e2428279459c6a3c53692aa011d43cc8c4
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-03-13 19:44:42 -0300

    Update

--- chatbot.js
+++ chatbot.js
@@ -70,6 +70,7 @@ bot.on('message', msg => {
         if (parentOption) {
             const subOption = parentOption.subOptions.find(sub => sub.option === text.toString());
             if (subOption) {
+                bot.sendMessage(chatId, `*\\[ ${subOption.option} \\]* ${escapeMarkdownV2(subOption.text)}`, { parse_mode: 'MarkdownV2' });
                 handleOptionSelection(chatId, subOption);
             } else {
                 bot.sendMessage(chatId, 'Opção inválida. Tente novamente.');
@@ -105,7 +106,7 @@ function sendFile(chatId, fileName, type, directory) {
     if (fs.existsSync(filePath)) {
         switch (type) {
             case 'audio':
-                bot.sendMessage(chatId, 'Enviando audio...');
+                bot.sendMessage(chatId, 'Enviando áudio...');
                 bot.sendAudio(chatId, filePath);
                 break;
             case 'document':
