15e302527d41942d440c8973c6b1a7306423309f
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-03-13 19:53:42 -0300

    Update

--- config.js
+++ config.js
@@ -1,29 +1,29 @@
 const menuOptions = [
     { 
         option: '1', 
-        text: 'Suporte Franquia', 
+        text: 'Opção 1', 
         type: 'text', 
         subOptions: [
-            { option: '1', text: 'Como abrir uma franquia', type: 'text' },
-            { option: '2', text: 'Como gerenciar uma franquia', type: 'text' },
-            { option: '3', text: 'Suporte técnico', type: 'text' }
+            { option: '1', text: 'Sub Opção 1', type: 'text' },
+            { option: '2', text: 'Sub Opção 2', type: 'text' },
+            { option: '3', text: 'Sub Opção 3', type: 'text' }
         ]
     },
     { 
         option: '2', 
-        text: 'Migração Franquia', 
+        text: 'Opção 2', 
         type: 'text', 
         subOptions: [
-            { option: '1', text: 'Como migrar uma franquia', type: 'text' },
-            { option: '2', text: 'Suporte para migração', type: 'text' }
+            { option: '1', text: 'Sub Opção 1', type: 'text' },
+            { option: '2', text: 'Sub Opção 2', type: 'text' }
         ]
     },
-    { option: '3', text: 'Duduzinho fofinho', type: 'text', subOptions: [] },
-    { option: '4', text: 'Carol dos docinhos', type: 'text', subOptions: [] },
-    { option: '5', text: 'Falar com suporte', type: 'text', subOptions: [] },
-    { option: '6', text: 'Enviar áudio para o cliente', type: 'audio', fileName: 'test.mp3', subOptions: [] },
-    { option: '7', text: 'Enviar documento para o cliente', type: 'document', fileName: 'test.pdf', subOptions: [] },
-    { option: '8', text: 'Enviar imagem para o cliente', type: 'image', fileName: 'test.jpg', subOptions: [] }
+    { option: '3', text: 'Opção 3', type: 'text', subOptions: [] },
+    { option: '4', text: 'Opção 4', type: 'text', subOptions: [] },
+    { option: '5', text: 'Opção 5', type: 'text', subOptions: [] },
+    { option: '6', text: 'Opção audio', type: 'audio', fileName: 'test.mp3', subOptions: [] },
+    { option: '7', text: 'Opção documento', type: 'document', fileName: 'test.pdf', subOptions: [] },
+    { option: '8', text: 'Opção imagem', type: 'image', fileName: 'test.jpg', subOptions: [] }
 ];
 
 module.exports = menuOptions;
