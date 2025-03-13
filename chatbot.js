f21d8f52bab4b8c09eb699005d6bdedfa2be7c68
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-03-13 13:09:39 -0300

    Upload

--- chatbot.js
+++ chatbot.js
@@ -28,28 +28,52 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(chatId, optionsMessage);
 });
 
-
 bot.onText(/.*/, (msg, match) => {
     const chatId = msg.chat.id;
     const option = match[0];
 
     switch (option) {
-        case '1':
-            bot.sendMessage(chatId, 'Você escolheu a Opção 1!');
+        case '1': // Opção 1: Suporte Franquia
+            bot.sendMessage(chatId, 'Você escolheu "Suporte Franquia". Escolha uma sub-opção:\n[1.1] Como abrir uma franquia\n[1.2] Como gerenciar uma franquia\n[1.3] Suporte técnico');
             break;
-        case '2':
-            bot.sendMessage(chatId, 'Você escolheu a Opção 2!');
+        
+        case '1.1': // Sub-opção 1.1: Como abrir uma franquia
+            bot.sendMessage(chatId, 'Para abrir uma franquia, entre em contato com nossa equipe de expansão ou visite nosso site para mais informações.');
             break;
-        case '3':
-            bot.sendMessage(chatId, 'Você escolheu a Opção 3!');
+        
+        case '1.2': // Sub-opção 1.2: Como gerenciar uma franquia
+            bot.sendMessage(chatId, 'Para gerenciar sua franquia, acesse o portal do franqueado e consulte os materiais de apoio.');
             break;
-        case '4':
-            bot.sendMessage(chatId, 'Você escolheu a Opção 4!');
+
+        case '1.3': // Sub-opção 1.3: Suporte técnico
+            bot.sendMessage(chatId, 'Se você precisa de suporte técnico, envie o problema detalhado e nossa equipe irá ajudar o mais rápido possível.');
             break;
-        case '5':
-            bot.sendMessage(chatId, 'Você escolheu a Opção 5!');
+
+        case '2': // Opção 2: Migração Franquia
+            bot.sendMessage(chatId, 'Você escolheu "Migração Franquia". Escolha uma sub-opção:\n[2.1] Como migrar uma franquia\n[2.2] Suporte para migração');
             break;
-        default:
+
+        case '2.1': // Sub-opção 2.1: Como migrar uma franquia
+            bot.sendMessage(chatId, 'Para migrar uma franquia, você deve seguir o processo de migração descrito em nosso portal de franqueados.');
+            break;
+
+        case '2.2': // Sub-opção 2.2: Suporte para migração
+            bot.sendMessage(chatId, 'Para obter suporte na migração da franquia, entre em contato com a nossa equipe técnica via e-mail ou telefone.');
+            break;
+
+        case '3': // Opção 3: Duduzinho fofinho
+            bot.sendMessage(chatId, 'Você escolheu "Duduzinho fofinho"! Não temos sub-opções para esta opção, mas podemos conversar sobre coisas fofinhas!');
+            break;
+
+        case '4': // Opção 4: Carol dos docinhos
+            bot.sendMessage(chatId, 'Você escolheu "Carol dos docinhos"! Fique à vontade para perguntar sobre nossos deliciosos docinhos!');
+            break;
+
+        case '5': // Opção 5: Falar com suporte
+            bot.sendMessage(chatId, 'Você escolheu "Falar com suporte". Um atendente entrará em contato com você em breve.');
+            break;
+
+        default: // Caso o usuário digite uma opção inválida
             bot.sendMessage(chatId, 'Opção inválida. Por favor, escolha um número válido.');
             break;
     }
